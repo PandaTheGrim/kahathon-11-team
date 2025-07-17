b271aa3ebef06a2851a3772c04aeb554123c1523
KADOCHNIKOV, Igor A. (Ext. - UniCredit Bank - RUS) <IKadochnikov.external@unicredit.ru>
2025-07-17 22:56:51 +0500

    - комментарии

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ from fastapi import FastAPI
 from dto.visit_data import VisitData
 
 app = FastAPI()
+# прогружаем нашу модель
 model = joblib.load('./model/model.pkl')
 
 
 @app.post("/predict")
 def predict(data: VisitData):
+    # тут складываем фичи, которые подаем на вход модели в нужном порядке
     features = [
         data.utm_source,
         data.geo_country,
         data.device_category
     ]
-
+    # вызываем "предсказывалку"
     pred = model.predict([features])[0]
-
+    # возвращаем 1 или 0
     return {"prediction_result": int(pred)}
