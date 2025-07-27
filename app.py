866823a88c5b4ffaba82dfed743d6ad23adc8068
yvrikgr <yyarik14@gmail.com>
2025-07-27 18:56:18 +0400

    Updated features app.py

--- app.py
+++ app.py
@@ -12,9 +12,24 @@ model = joblib.load('./model/model.pkl')
 def predict(data: VisitData):
     # тут складываем фичи, которые подаем на вход модели в нужном порядке
     features = [
-        data.utm_source,
-        data.geo_country,
-        data.device_category
+        data.go_to_car_card,
+        data.pagination_click,
+        data.photos,
+        data.photos_all,
+        data.sap_search_form_cost_from,
+        data.sap_search_form_cost_to,
+        data.search_form_mark_select,
+        data.search_form_model_select,
+        data.search_form_region,
+        data.search_form_search_btn,
+        data.search_form_search_car_type_select,
+        data.search_kpp,
+        data.sub_car_page,
+        data.sub_landing,
+        data.sub_view_cars_click,
+        data.view_card,
+        data.view_more_click,
+        data.view_new_card
     ]
     # вызываем "предсказывалку"
     pred = model.predict([features])[0]
