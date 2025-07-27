70aa2b541013a6a8006b60eef843e860f831558a
yvrikgr <yyarik14@gmail.com>
2025-07-27 18:57:34 +0400

    Updated parameters visit_data.py

--- dto/visit_data.py
+++ dto/visit_data.py
@@ -3,24 +3,21 @@ from pydantic import BaseModel
 
 # TODO расширить по необходимости входными параметрами, актуализировать названия
 class VisitData(BaseModel):
-    go_to_car_card: str
-    pagination_click: str
-    photos: str
-    photos_all: str
-    quiz_show: str
-    sap_search_form_cost_from: str
-    sap_search_form_cost_to: str
-    search_form_mark_select: str
-    search_form_model_select: str
-    search_form_region: str
-    search_form_search_btn: str
-    search_form_search_car_type_select: str
-    search_kpp: str
-    showed_number_ads: str
-    sub_car_page: str
-    sub_landing: str
-    sub_view_cars_click: str
-    view_card: str
-    view_more_click: str
-    view_new_card: str
-    custom_target: str
+    go_to_car_card: conint(ge=0, le=1)
+    pagination_click: conint(ge=0, le=1)
+    photos: conint(ge=0, le=1)
+    photos_all: conint(ge=0, le=1)
+    sap_search_form_cost_from: conint(ge=0, le=1)
+    sap_search_form_cost_to: conint(ge=0, le=1)
+    search_form_mark_select: conint(ge=0, le=1)
+    search_form_model_select: conint(ge=0, le=1)
+    search_form_region: conint(ge=0, le=1)
+    search_form_search_btn: conint(ge=0, le=1)
+    search_form_search_car_type_select: conint(ge=0, le=1)
+    search_kpp: conint(ge=0, le=1)
+    sub_car_page: conint(ge=0, le=1)
+    sub_landing: conint(ge=0, le=1)
+    sub_view_cars_click: conint(ge=0, le=1)
+    view_card: conint(ge=0, le=1)
+    view_more_click: conint(ge=0, le=1)
+    view_new_card: conint(ge=0, le=1)
